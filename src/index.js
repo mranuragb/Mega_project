6d8d404f3bcb266d33e56d786dba63b1e0192a36
mranuragb <bawaneanurag@gmail.com>
2024-02-06 20:39:08 +0530

    cors-origin

--- src/index.js
+++ src/index.js
@@ -12,8 +12,14 @@ dotenv.config({
 
 // 2ND Approach
 connectDB()
-
-
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port :${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDb connection Failed !!",err);
+})
 
 
 /*
