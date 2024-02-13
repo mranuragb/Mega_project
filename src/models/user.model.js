280548618f88502b08410a5825511cdff4387c4a
mranuragb <bawaneanurag@gmail.com>
2024-02-14 00:21:37 +0530

    add User model & video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,6 +57,28 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken = async function(){}
-userSchema.methods.generateRefreshToken = async function(){}
+userSchema.methods.generateAccessToken = async function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+userSchema.methods.generateRefreshToken = async function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
 export const User = mongoose.model("User",userSchema);
